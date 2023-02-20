1dbc90c180bff1896e7ab8913c1c6a8cb055fcb2
andefined <dimitris.papaevagelou@andefined.com>
2023-02-20 22:17:00 +0200

    omitemty fields

--- models/relationships/model.go
+++ models/relationships/model.go
@@ -3,13 +3,13 @@ package relationships
 type NodeArticle struct {
 	Uid         string         `json:"uid"`
 	DocId       string         `json:"doc_id"`
-	Title       string         `json:"title"`
-	Lang        string         `json:"lang"`
-	CrawledAt   string         `json:"crawled_at"`
-	PublishedAt string         `json:"published_at"`
-	Url         string         `json:"url"`
-	ScreenName  string         `json:"screen_name"`
-	Type        string         `json:"type"`
+	Title       string         `json:"title,omitempty"`
+	Lang        string         `json:"lang,omitempty"`
+	CrawledAt   string         `json:"crawled_at,omitempty"`
+	PublishedAt string         `json:"published_at,omitempty"`
+	Url         string         `json:"url,omitempty"`
+	ScreenName  string         `json:"screen_name,omitempty"`
+	Type        string         `json:"type,omitempty"`
 	RelCount    int64          `json:"relCount,omitempty"`
 	Similar     []*NodeArticle `json:"similar,omitempty"`
 	Score       float64        `json:"score,omitempty"`
@@ -17,15 +17,15 @@ type NodeArticle struct {
 
 type NodeFeed struct {
 	Uid        string `json:"uid"`
-	FeedId     string `json:"feed_id"`
-	Name       string `json:"name"`
-	ScreenName string `json:"screen_name"`
-	Url        string `json:"url"`
-	Type       string `json:"type"`
+	FeedId     string `json:"feed_id,omitempty"`
+	Name       string `json:"name,omitempty"`
+	ScreenName string `json:"screen_name,omitempty"`
+	Url        string `json:"url,omitempty"`
+	Type       string `json:"type,omitempty"`
 }
 
 type NodeEntity struct {
 	Uid   string `json:"uid"`
-	Label string `json:"label"`
-	Type  string `json:"type"`
+	Label string `json:"label,omitempty"`
+	Type  string `json:"type,omitempty"`
 }
