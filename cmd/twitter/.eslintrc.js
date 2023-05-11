71640efbfae14860977942ecd95313c630fb2e90
andefined <dimitris.papaevagelou@andefined.com>
2023-05-11 13:15:44 +0300

    add support for v1

--- cmd/twitter/.eslintrc.js
+++ cmd/twitter/.eslintrc.js
@@ -1,39 +1,40 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true
-    },
-    extends: [
-        'airbnb-base'
-    ],
-    rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        // allow debugger during development
-        'space-before-function-paren': ['error', 'always'],
-        // allow paren-less arrow functions
-        'arrow-parens': 0,
-        // allow async-await
-        'generator-star-spacing': 0,
-        // allow debugger during development
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-        'no-mixed-spaces-and-tabs': 0,
-        // tabs
-        indent: 0,
-        'no-tabs': 0,
-        semi: ['error', 'always'],
-        'comma-dangle': 0,
-        'consistent-return': 0,
-        'function-paren-newline': ['error', 'never'],
-        'implicit-arrow-linebreak': ['off'],
-        'no-param-reassign': 0,
-        'no-underscore-dangle': 0,
-        'no-shadow': 0,
-        'no-plusplus': 0,
-        'no-unused-expressions': 0,
-    },
-    parserOptions: {
-        parser: 'babel-eslint',
-        sourceType: 'module',
-        ecmaVersion: 8
-    }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: [
+		'airbnb-base'
+	],
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		// allow debugger during development
+		'space-before-function-paren': ['error', 'always'],
+		// allow paren-less arrow functions
+		'arrow-parens': 0,
+		// allow async-await
+		'generator-star-spacing': 0,
+		// allow debugger during development
+		'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+		'no-mixed-spaces-and-tabs': 0,
+		// tabs
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+		semi: ['error', 'always'],
+		'comma-dangle': 0,
+		'consistent-return': 0,
+		'function-paren-newline': ['error', 'never'],
+		'implicit-arrow-linebreak': ['off'],
+		'no-param-reassign': 0,
+		'no-underscore-dangle': 0,
+		'no-shadow': 0,
+		'no-plusplus': 0,
+		'no-unused-expressions': 0,
+		camelcase: 0
+	},
+	parserOptions: {
+		parser: 'babel-eslint',
+		sourceType: 'module',
+		ecmaVersion: 8
+	}
 };
--- cmd/twitter/src/index.js
+++ cmd/twitter/src/index.js
@@ -1,13 +1,9 @@
-import {
-  ETwitterStreamEvent,
-  TwitterApi,
-} from 'twitter-api-v2';
-
 require('dotenv').config();
 
 const { Kafka } = require('kafkajs');
 const url = require('url');
 const { v4: uuidv4 } = require('uuid');
+const { ETwitterStreamEvent, TwitterApi } = require('twitter-api-v2');
 const logger = require('./logger');
 
 const token = process.env.TWITTER_BEARER_TOKEN;
@@ -20,102 +16,122 @@ const kafka = new Kafka({ clientId, brokers });
 const producer = kafka.producer();
 
 const isValidUrl = (s) => {
-  if (s.includes('twitter.com')) return false;
-  if (s.includes('live24.gr')) return false;
-  if (s.includes('indymedia.org')) return false;
-  if (s.includes('.jpg') || s.includes('.png')) return false;
+	if (s.includes('twitter.com')) return false;
+	if (s.includes('live24.gr')) return false;
+	if (s.includes('indymedia.org')) return false;
+	if (s.includes('.jpg') || s.includes('.png')) return false;
 
-  const parts = url.parse(s);
-  if (parts.path.length <= 1) return false;
+	const parts = url.parse(s);
+	if (parts.path.length <= 1) return false;
 
-  return true;
+	return true;
 };
 
 const getUserNameFromTweet = (authorId, users) => {
-  for (let i = 0; i < users.length; i++) {
-    if (authorId === users[i].id) return users[i].username;
-  }
+	for (let i = 0; i < users.length; i++) {
+		if (authorId === users[i].id) return users[i].username;
+	}
 
-  return '';
+	return '';
+};
+
+const getMessage = (data, url, username) => {
+	if (process.env.VERSION === 'v1') {
+		return {
+			docId: uuidv4(),
+			url,
+			tweet_id: data.id,
+			twitter_user_id: parseInt(data.author_id, 16),
+			twitter_user_id_str: data.author_id.toString(),
+			screen_name: username,
+			created_at: new Date(data.created_at),
+			created_at_str: data.created_at
+
+		};
+	}
+	return {
+		docId: uuidv4(),
+		type: 'twitter',
+		url,
+		tweet_id: data.id,
+		twitter_user_id: data.author_id,
+		created_at: data.created_at,
+		user_name: username
+	};
 };
 
 const produce = async (msg) => {
-  try {
-    // send a message to the configured topic with
-    // the key and value formed from the current value of `i`
-    await producer.send({
-      topic,
-      messages: [
-        {
-          value: JSON.stringify(msg)
-        },
-      ],
-    });
+	try {
+		// send a message to the configured topic with
+		// the key and value formed from the current value of `i`
+		await producer.send({
+			topic,
+			messages: [
+				{
+					value: JSON.stringify(msg)
+				},
+			],
+		});
 
-    logger.info(`New Tweet: ${msg.user_name} ${msg.url}`);
-  } catch (err) {
-    logger.error(`could not write message ${err}`);
-  }
+		logger.info(`New Tweet: ${msg.user_name} ${msg.url}`);
+	} catch (err) {
+		logger.error(`could not write message ${err}`);
+	}
 };
 
 const main = async () => {
-  logger.info('Starting Service');
-  await producer.connect();
+	logger.info('Starting Service');
+	await producer.connect();
 
-  const stream = await twitterClient.v2.searchStream({
-    'tweet.fields':
+	const stream = await twitterClient.v2.searchStream({
+		'tweet.fields':
 			'created_at,id,author_id,lang,entities,in_reply_to_user_id'.split(','),
-    'user.fields': 'id,name,profile_image_url,url,username,verified'.split(','),
-    expansions: 'author_id,attachments.media_keys'.split(',')
-  });
+		'user.fields': 'id,name,profile_image_url,url,username,verified'.split(','),
+		expansions: 'author_id,attachments.media_keys'.split(',')
+	});
 
-  stream.on(ETwitterStreamEvent.ConnectionError,
-    err => logger.error('Connection error!', err));
+	stream.on(ETwitterStreamEvent.ConnectionError,
+		err => logger.error('Connection error!', err));
 
-  stream.on(ETwitterStreamEvent.ConnectionClosed,
-    () => logger.info('Connection has been closed.'));
+	stream.on(ETwitterStreamEvent.ConnectionClosed,
+		() => logger.info('Connection has been closed.'));
 
-  stream.on(ETwitterStreamEvent.Data,
-    data => {
-      // stop
-      if (!data.data) return;
-      if (!data.data.entities) return;
-      if (!data.data.entities.urls) return;
-      if (!data.includes) return;
-      if (data.data.in_reply_to_user_id) return;
-      if (data.data.entities.urls.length <= 0) return;
+	stream.on(ETwitterStreamEvent.Data,
+		data => {
+			console.debug(data);
+			// stop
+			if (!data.data) return;
+			if (!data.data.entities) return;
+			if (!data.data.entities.urls) return;
+			if (!data.includes) return;
+			if (data.data.in_reply_to_user_id) return;
+			if (data.data.entities.urls.length <= 0) return;
 
-      const urls = JSON.parse(JSON.stringify(data.data.entities));
-      for (let i = 0; i < urls.urls.length; i++) {
-        const u = urls.urls[i];
-        if (u.expanded_url && isValidUrl(u.expanded_url)) {
-          const username = getUserNameFromTweet(data.data.author_id, data.includes.users);
-          if (username !== '') {
-            const message = {
-              docId: uuidv4(),
-              type: 'twitter',
-              url: u.expanded_url,
-              tweet_id: data.data.id,
-              twitter_user_id: data.data.author_id,
-              created_at: data.data.created_at,
-              user_name: username
-            };
+			const matchingRules = data.matching_rules.map(m => m.tag);
+			if (!matchingRules.includes(`mediawatch-listener-${process.env.VERSION}`)) return;
 
-            produce(message);
-          }
-        }
-      }
-    });
+			// process
+			const urls = JSON.parse(JSON.stringify(data.data.entities));
+			for (let i = 0; i < urls.urls.length; i++) {
+				const u = urls.urls[i];
+				if (u.expanded_url && isValidUrl(u.expanded_url)) {
+					const username = getUserNameFromTweet(data.data.author_id, data.includes.users);
+					if (username !== '') {
+						produce(getMessage(data.data, u.expanded_url, username));
+					}
+				}
+			}
+		});
 
-  stream.on(ETwitterStreamEvent.DataKeepAlive,
-    () => logger.debug('Twitter has a keep-alive packet.'));
+	stream.on(ETwitterStreamEvent.DataKeepAlive,
+		() => logger.debug('Twitter has a keep-alive packet.'));
 };
 
 const shutdown = err => {
-  if (err) {
-    logger.error(`Service error: ${err.message}`);
-  }
-  process.exit(err ? 1 : 0);
+	if (err) {
+		logger.error(`Service error: ${err.message}`);
+	}
+	process.exit(err ? 1 : 0);
 };
 
 process.on('unhandledRejection', shutdown);
