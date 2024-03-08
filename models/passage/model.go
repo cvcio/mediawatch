90ee85c2aa393674e917d14d300a8a077e9132df
giannis panagiotopoulos <giannis.panagiotopoulos@gmail.com>
2024-03-08 20:09:56 +0200

    update passages model

--- models/passage/model.go
+++ models/passage/model.go
@@ -1,12 +1,15 @@
 package passage
 
 import (
+	passagesv2 "github.com/cvcio/mediawatch/pkg/mediawatch/passages/v2"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // Passage to Trim
+// Passage to Trim
 type Passage struct {
-	ID   primitive.ObjectID `bson:"_id" json:"id"`
-	Text string             `json:"text" bson:"text"`
-	Type string             `json:"type" bson:"type"`
+	ID       primitive.ObjectID     `bson:"_id" json:"id"`
+	Text     string                 `json:"text" bson:"text"`
+	Language string                 `json:"language" bson:"language"`
+	Type     passagesv2.PassageType `json:"type" bson:"type"`
 }
