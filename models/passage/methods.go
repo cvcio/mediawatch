b725c96b00d0e61314b930f677cb6adfd4b9db19
giannis panagiotopoulos <giannis.panagiotopoulos@gmail.com>
2024-03-08 21:54:15 +0200

    refactor create method for consistency

--- models/passage/methods.go
+++ models/passage/methods.go
@@ -8,10 +8,8 @@ import (
 	"github.com/cvcio/mediawatch/pkg/db"
 	passagesv2 "github.com/cvcio/mediawatch/pkg/mediawatch/passages/v2"
 	"github.com/pkg/errors"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.opencensus.io/trace"
 )
 
 const passagesCollection = "passages"
@@ -19,24 +17,30 @@ const passagesCollection = "passages"
 // Create inserts a new passage into the database.
 func Create(ctx context.Context, dbConn *db.MongoDB, passage *passagesv2.Passage, now time.Time) (*passagesv2.Passage, error) {
 
-	ctx, span := trace.StartSpan(ctx, "model.passage.Create")
-	defer span.End()
-
-	filter := bson.M{"type": passage.Type, "text": passage.Text}
-
-	var p *passagesv2.Passage
-
 	f := func(collection *mongo.Collection) error {
-		return collection.FindOne(ctx, filter).Decode(&p)
-	}
-	passage.Id = primitive.NewObjectID().Hex()
+		inserted, err := collection.InsertOne(ctx, &passage) // (&u)
+		// Normally we would return ErrDuplicateKey in this scenario but we do not want
+		// to leak to an unauthenticated user which emails are in the system.
+		if err != nil {
+			we, _ := err.(mongo.WriteException)
+			if we.WriteErrors[0].Code == 11000 {
+				return db.ErrExists
+			}
 
-	f = func(collection *mongo.Collection) error {
-		_, err := collection.InsertOne(ctx, &passage)
-		return err
+			return err
+		}
+
+		if oid, ok := inserted.InsertedID.(primitive.ObjectID); ok {
+			passage.Id = oid.Hex()
+		} else {
+			return db.ErrInvalid
+		}
+
+		return nil
 	}
+
 	if err := dbConn.Execute(ctx, passagesCollection, f); err != nil {
-		return nil, errors.Wrap(err, fmt.Sprintf("db.passages.insert(%s)", db.Query(&passage)))
+		return nil, errors.Wrap(err, fmt.Sprintf("db.passages.insert(%s)", passage.Text))
 	}
 
 	return passage, nil
